43b5ec2e5d440522b31150ffda1c19d7e873d75d
Abir Charfi <abir8charfi@gmail.com>
2023-11-15 21:28:08 +0100

    update

--- React/Core/moreForms/more-forms/src/components/Form.jsx
+++ React/Core/moreForms/more-forms/src/components/Form.jsx
@@ -14,27 +14,36 @@ const Form = () => {
         <div>
           <label>First name</label>
           <input type="text" name="firstName" value={firstName} onChange={(e) => { setFirstName(e.target.value) }} />
-          {firstName.length <3 ? <p>First name should be at least 3 character</p> : ''}
+          {(firstName !== '' && firstName.length < 3) ? (
+  <p>First name should be at least 3 characters</p>
+) : null}
         </div>
         <div>
           <label>Last Name</label>
           <input type="text" name="lastName" value={lastName} onChange={(e) => { setLastName(e.target.value)}} />
-          {lastName.length <3 ? <p>Last name should be at least 3 characters</p> : ''}
+          {(lastName !== '' && lastName.length < 3) ? (
+  <p>Last name should be at least 3 characters</p>
+) : null}
         </div>
         <div>
           <label>Email</label>
           <input type="email" name="email" value={email} onChange={(e) => { setEmail(e.target.value) }} />
-          {email.length <3 ? <p>Email should be at least 3 characters</p> : ''}
+          {(email !== '' && email.length < 3) ? (
+  <p>Email should be at least 3 characters</p>
+) : null}
         </div>
         <div>
           <label>Password</label>
           <input type="password" name="password" value={password} onChange={(e) => { setPassword(e.target.value) }} />
-          {password.length <8 ? <p>Password should be at least 8 characters</p> : ''}
+          {(password !== '' && password.length < 8) ? (
+  <p>Password should be at least 8 characters</p>
+) : null}
         </div>
         <div>
           <label>Confirm Password</label>
           <input type="password" name="confirmPassword" value={confirmPassword} onChange={(e) =>{ setConfirmPassword(e.target.value) }} />
-          {password !== confirmPassword ? <p>Passwords must much !</p> : ''}
+          {( confirmPassword !== '' && password !== confirmPassword )? (
+          <p>Passwords must much !</p>) : null}
         </div>
       </form>
       
