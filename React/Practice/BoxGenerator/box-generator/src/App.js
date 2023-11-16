7011ecbb269f033ef4b8cbaaa7d76888b3dba03b
Abir Charfi <abir8charfi@gmail.com>
2023-11-16 15:28:17 +0100

    'update'

---
+++ React/Practice/BoxGenerator/box-generator/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Color from './components/Color'
+
+function App() {
+  return (
+    <div className="App">
+
+      <Color></Color>
+      
+    </div>
+  );
+}
+
+export default App;
