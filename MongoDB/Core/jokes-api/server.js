f8638a34897d4bc73be43aacad1eaf43bb6c8707
Abir Charfi <abir8charfi@gmail.com>
2023-12-03 15:41:18 +0100

    update

---
+++ MongoDB/Core/jokes-api/server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+
+const app = express()
+
+
+app.use(express.json());
+require("dotenv").config()
+require("./config/mongoose.config")
+
+const port = process.env.PORT
+
+const Routes = require("./routes/joke.routes")
+Routes(app)
+
+
+app.listen(port, () => {
+    console.log(`>>>>> Server is running on Port ${port}`)
+})
